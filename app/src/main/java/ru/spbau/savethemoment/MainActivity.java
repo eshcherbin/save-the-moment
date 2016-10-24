f3674595ea19b0225bcb8c79770dc06bfb9dd2c9
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-10-24 23:20:20 +0700

    Created three buttons with empty click handlers

--- app/src/main/java/ru/spbau/savethemoment/MainActivity.java
+++ app/src/main/java/ru/spbau/savethemoment/MainActivity.java
@@ -2,6 +2,8 @@ package ru.spbau.savethemoment;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,30 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // set button click handlers
+        Button main_list_button = (Button) findViewById(R.id.button_main_list);
+        main_list_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: handle click (start List of moments activity)
+            }
+        });
+
+        Button main_map_button = (Button) findViewById(R.id.button_main_map);
+        main_map_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: handle click (start Map of moments activity)
+            }
+        });
+
+        Button main_create_button = (Button) findViewById(R.id.button_main_create);
+        main_create_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO: handle click (Create new moment and)
+            }
+        });
     }
 }
