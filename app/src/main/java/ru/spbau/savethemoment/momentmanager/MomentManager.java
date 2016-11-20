b170227119e121a2a211d21ed31507ca59190649
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-11-20 21:03:22 +0700

    Started implementing MomentManager as a wrapper around a SQLite Database

--- app/src/main/java/ru/spbau/savethemoment/momentmanager/MomentManager.java
+++ app/src/main/java/ru/spbau/savethemoment/momentmanager/MomentManager.java
@@ -1,15 +1,13 @@
 package ru.spbau.savethemoment.momentmanager;
 
-import android.content.ContentProvider;
-import android.content.ContentValues;
 import android.content.Context;
-import android.content.UriMatcher;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.net.Uri;
 
-public class MomentManager extends ContentProvider {
+import java.util.Set;
+
+public class MomentManager {
 
     private static final String DB_NAME = "moments_db";
     private static final int DB_VERSION = 1;
@@ -24,9 +22,12 @@ public class MomentManager extends ContentProvider {
     private static final String MOMENT_LOCATION_LONGITUDE = "location_longitude";
     private static final String MOMENT_LOCATION_LATITUDE = "location_latitude";
 
-    private static final String TAG_ID = "_ID";
     private static final String TAG_MOMENT_ID = "moment_id";
     private static final String TAG_NAME = "name";
+
+    private static final String[] MOMENT_COLUMNS = new String[]{MOMENT_ID, MOMENT_TITLE,
+            MOMENT_DESCRIPTION, MOMENT_CAPTURING_TIME,
+            MOMENT_LOCATION_LONGITUDE, MOMENT_LOCATION_LATITUDE};
 
     private static final String DB_MOMENTS_CREATE = "create table " + MOMENTS_TABLE + "("
             + MOMENT_ID + " integer primary key autoincrement, "
@@ -36,88 +37,43 @@ public class MomentManager extends ContentProvider {
             + MOMENT_LOCATION_LONGITUDE + "double, "
             + MOMENT_LOCATION_LATITUDE + "double" + ");";
     private static final String DB_TAGS_CREATE = "create table " + TAGS_TABLE + "("
-            + TAG_ID + " integer primary key autoincrement, "
             + TAG_MOMENT_ID + " integer, "
             + TAG_NAME + "text\n"
             + "foreign key (" + TAG_MOMENT_ID + ") references "
             + MOMENTS_TABLE + "(" + MOMENT_ID + ")" + ");";
 
-    private static final String AUTHORITY = "ru.spbau.savethemoment.momentmanager";
-
-    private static final String MOMENT_PATH = "moments";
-    private static final String TAG_PATH = "tags";
-
-    private static final String MOMENT_CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + AUTHORITY
-            + "." + MOMENT_PATH;
-    private static final String MOMENT_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd."
-            + AUTHORITY + "." + MOMENT_PATH;
-    private static final String TAG_CONTENT_TYPE = "vnd.android.cursor.dir/vnd." + AUTHORITY
-            + "." + TAG_PATH;
-    private static final String TAG_CONTENT_ITEM_TYPE = "vnd.android.cursor.item/vnd."
-            + AUTHORITY + "." + TAG_PATH;
-
-    private static final int URI_MOMENTS = 1;
-    private static final int URI_MOMENTS_ID = 2;
-    private static final int URI_TAGS = 3;
-    private static final int URI_TAGS_ID = 4;
-
-    private static final UriMatcher uriMatcher;
-    static {
-        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMatcher.addURI(AUTHORITY, MOMENT_PATH, URI_MOMENTS);
-        uriMatcher.addURI(AUTHORITY, MOMENT_PATH + "/#", URI_MOMENTS_ID);
-        uriMatcher.addURI(AUTHORITY, TAG_PATH, URI_TAGS);
-        uriMatcher.addURI(AUTHORITY, TAG_PATH + "/#", URI_TAGS_ID);
-    }
-
     private DBHelper dbHelper;
-    private SQLiteDatabase sqLiteDatabase;
 
-    public static final Uri MOMENTS_CONTENT_URI = Uri.parse("content://" + AUTHORITY +
-            "/" + MOMENT_PATH);
-    public static final Uri TAGS_CONTENT_URI = Uri.parse("content://" + AUTHORITY +
-            "/" + TAG_PATH);
-
-    public MomentManager() {
+    public MomentManager(Context context) {
+        dbHelper = new DBHelper(context);
     }
 
-    @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
-        // Implement this to handle requests to delete one or more rows.
+    public Cursor getMoments() {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        return database.query(MOMENTS_TABLE, MOMENT_COLUMNS, null, null, null, null, null);
+    }
+
+    public Cursor getTagsByMomentId(int momentId) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        return database.query(TAGS_TABLE, new String[]{TAG_NAME}, TAG_MOMENT_ID + "=?",
+                new String[]{String.valueOf(momentId)}, null, null, null);
+    }
+
+    public Cursor getMomentsByTags(Set<String> tags) {
+        SQLiteDatabase database = dbHelper.getReadableDatabase();
+        //TODO: implement querying by tags as in issue #5
         throw new UnsupportedOperationException("Not yet implemented");
     }
 
-    @Override
-    public String getType(Uri uri) {
-        // TODO: Implement this to handle requests for the MIME type of the data
-        // at the given URI.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void deleteMomentById(int momentId) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        database.delete(TAGS_TABLE, TAG_MOMENT_ID + "=?", new String[]{String.valueOf(momentId)});
+        database.delete(MOMENTS_TABLE, MOMENT_ID + "=?", new String[]{String.valueOf(momentId)});
     }
 
-    @Override
-    public Uri insert(Uri uri, ContentValues values) {
-        // TODO: Implement this to handle requests to insert a new row.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public boolean onCreate() {
-        // TODO: Implement this to initialize your content provider on startup.
-        return false;
-    }
-
-    @Override
-    public Cursor query(Uri uri, String[] projection, String selection,
-                        String[] selectionArgs, String sortOrder) {
-        // TODO: Implement this to handle query requests from clients.
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
-
-    @Override
-    public int update(Uri uri, ContentValues values, String selection,
-                      String[] selectionArgs) {
-        // TODO: Implement this to handle requests to update one or more rows.
-        throw new UnsupportedOperationException("Not yet implemented");
+    public void insertMoment(Moment moment) {
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+        //TODO: implement inserting a moment and all its tags
     }
 
     private static class DBHelper extends SQLiteOpenHelper {
@@ -128,7 +84,8 @@ public class MomentManager extends ContentProvider {
 
         @Override
         public void onCreate(SQLiteDatabase db) {
-
+            db.execSQL(DB_MOMENTS_CREATE);
+            db.execSQL(DB_TAGS_CREATE);
         }
 
         @Override
