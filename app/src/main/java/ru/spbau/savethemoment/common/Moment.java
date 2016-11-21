3dee1eca00be00f60dd72c56604889aa72ab1ec2
egor-bogomolov <msicar.ot.on.yas@gmail.com>
2016-11-21 15:57:37 +0300

    Added classes Moment and MediaContent to package "common" in development branch

---
+++ app/src/main/java/ru/spbau/savethemoment/common/Moment.java
@@ -0,0 +1,50 @@
+package ru.spbau.savethemoment.common;
+
+import android.location.Location;
+
+import java.io.Serializable;
+import java.util.Calendar;
+import java.util.List;
+
+public class Moment implements Serializable {
+    private String id;
+    private String title;
+    private String description;
+    private Calendar capturingTime;
+    private Location location;
+    private List<String> tags;
+
+    public Moment(String id, String title, String description, Calendar capturingTime,
+                  Location location, List<String> tags) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.capturingTime = capturingTime;
+        this.location = location;
+        this.tags = tags;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Calendar getCapturingTime() {
+        return capturingTime;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+}
