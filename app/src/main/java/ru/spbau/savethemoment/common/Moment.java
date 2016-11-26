f0e1e791e8b9f07d20849a8cd50fa926b84e9b52
Egor Lord_F Shcherbin <javamanse@ya.ru>
2016-11-26 18:31:05 +0700

    Resolve conflicts

--- app/src/main/java/ru/spbau/savethemoment/common/Moment.java
+++ app/src/main/java/ru/spbau/savethemoment/common/Moment.java
@@ -4,7 +4,6 @@ import android.location.Location;
 
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.List;
 import java.util.Set;
 
 public class Moment implements Serializable {
